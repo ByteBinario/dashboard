3632d0d5fe6759f6771c7cf70f5e6517469974b0
Douglas Maia <douglasmai4@protonmail.com>
2023-11-19 20:15:36 -0300

    feat: Login page creation

--- pages/login.vue
+++ pages/login.vue
@@ -1,24 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const supabase = useSupabaseClient()
+
 import Input from '../components/Input.vue'
 import Button from '../components/Button.vue'
 
 const email = ref('')
 const password = ref('')
 const loading = ref(false)
+
+async function handleLogin(e) {
+  e.preventDefault()
+
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithPassword({ email: email.value, password: password.value })
+    if (error) throw error
+  } catch (error) {
+    alert(error.error_description || error.message)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
   <main>
     <section>
       <h1>Faça login para continuar</h1>
-      <form>
+      <form @submit="handleLogin">
         <Input
           type="email"
           id="email-input"
           icon="ph:at"
           v-model="email"
+          placeholder="Email"
           required
         />
 
@@ -27,17 +44,22 @@ const loading = ref(false)
           id="password-input"
           icon="ph:lock"
           v-model="password"
+          placeholder="Senha"
           required
         />
 
         <Button
           type="submit"
           :loading="loading"
-          @click="loading = !loading"
         >
           Entrar
         </Button>
       </form>
+
+      <article>
+        <p>Não possuí uma conta?</p>
+        <p>Então <NuxtLink to="/register">clique aqui</NuxtLink> e crie uma!</p>
+      </article>
     </section>
   </main>
 </template>
@@ -63,6 +85,19 @@ main {
       flex-direction: column;
       gap: 0.5rem;
       margin-top: 1rem;
+    }
+
+    article {
+      background-color: $bg-color;
+      margin-top: 0.5rem;
+      padding: 1rem;
+      text-align: center;
+      font-size: 1rem;
+      border-radius: 10px;
+
+      a {
+        color: $c-secondary;
+      }
     }
   }
 }
